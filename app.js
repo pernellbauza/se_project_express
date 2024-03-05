c8a11a000aaba9faed394341b3b2c30c1287c661
Pernell Bauza <pernellbauza@gmail.com>
2024-03-04 19:25:15 -0800

    error constructors added

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 app.use(cors());
 
 app.post("/signin", validateAuth, login);
